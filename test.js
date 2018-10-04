1e01fe611b3a68137be0e867c937789f8d347074
Brian Mearns <mearns.b@gmail.com>
2018-10-03 21:42:25 -0400

    Fix some bugs

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 const cork = require('.')
 
-const logger = cork(entry => console.log(entry))
+const logger = cork(entry => console.log(entry), {
+  name: 'test-logger',
+  decorations: { alwaysThere: 'yes it is' },
+  threshold: 'warning',
+  middleware: [
+    entry => ({
+      ...entry,
+      level: entry.level + 100
+    })
+  ]
+})
 
 ;['trace', 'debug', 'info', 'warn', 'warning', 'error', 'critical', 'XXX'].forEach((level, idx) => {
   logger[level](`This is ${level}`, { level }, { idx })
 })
+
+logger.push({ name: 'child-logger' })
+logger.error('Child logging')
